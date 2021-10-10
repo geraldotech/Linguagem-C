53dd0e98db5cd91e64807e15bae906997a955a23
tabuada <geraldo.filho92@hotmail.com>
2021-10-10 13:47:59 -0300

    diferenca entre 2 inteiros

---
+++ Diferenca entre 2N/2 numeros diferenca aleatoria.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+//numero aleatorio pois foi calculado a e b antes de receber valores
+void main(){
+int a, b, c;
+c = a-b;
+printf("Entre com dois inteiros:\n");
+scanf("%d %d", &a, &b);
+printf("A diferenca entre %d e %d vale %d\n", a, b, c);
+}
