ab745e374c812408c11e683cb862b27f6dc5a196
tabuada <geraldo.filho92@hotmail.com>
2021-10-10 13:00:29 -0300

    tabuada

---
+++ Tabuada/Tabuada.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+//byGeraldo-Tech
+void main () {
+int cont, res, opt;
+do
+{
+printf("TABUADA DE QUALQUER NUMERO:\n");
+printf("Insira um valor: ");
+scanf (" %i", &res);
+for (cont=1;cont<=10;cont=cont+1)
+    printf("%d x %d = %i\n",res ,cont, cont*res);
+
+printf("1 - continuar\n0 - sair \n");
+scanf (" %i", &opt);
+}
+while (opt==1);
+return 0;
+}
